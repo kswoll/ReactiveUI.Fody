a4eb1f6c57a6bcf8ec1bb3328fd22c033628c21d
koffmoff <fredrikhall74@gmail.com>
2016-04-16 15:54:50 +0200

    Added a test case and fix for #13 (#14)

---
+++ ReactiveUI.Fody.Tests/Issues/Issue13Tests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveUI.Fody.Tests.Issues
+{
+    [TestFixture]
+    public class Issue13Tests
+    {
+        [Test]
+        public void AccessingAChainedObservableAsPropertyOfDoubleDoesntThrow()
+        {
+            var vm = new VM();
+            Assert.AreEqual(0.0, vm.P2);
+        }
+
+        class VM : ReactiveObject
+        {
+            [ObservableAsProperty] public double P1 { get; }
+            [ObservableAsProperty] public double P2 { get; }
+
+            public VM()
+            {
+                Observable.Return(0.0).ToPropertyEx(this, vm => vm.P1);
+                this.WhenAnyValue(vm => vm.P1).ToPropertyEx(this, vm => vm.P2);
+            }
+        }
+
+    }
+
+}
