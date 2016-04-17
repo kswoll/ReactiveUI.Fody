eaaaa25079f03af42843ca5b11c6c917d668bc76
Kirk Woll <kirk.woll@gmail.com>
2016-04-17 07:33:16 -0700

    Fixes #11: Allow ObservableAsProperty on property accessors (#16)
    * Fixes #11: Allow ObservableAsProperty on property accessors

---
+++ ReactiveUI.Fody.Tests/Issues/Issue11Tests.cs
@@ -0,0 +1,27 @@
+﻿using System.Reactive.Linq;
+using NUnit.Framework;
+using ReactiveUI.Fody.Helpers;
+
+namespace ReactiveUI.Fody.Tests.Issues
+{
+    [TestFixture]
+    public class Issue11Tests
+    {
+        [Test]
+        public void AllowObservableAsPropertyAttributeOnAccessor()
+        {
+            var model = new TestModel("foo");
+            Assert.AreEqual("foo", model.MyProperty);
+        }
+
+        public class TestModel : ReactiveObject
+        {
+            public extern string MyProperty { [ObservableAsProperty]get; }
+
+            public TestModel(string myProperty)
+            {
+                Observable.Return(myProperty).ToPropertyEx(this, x => x.MyProperty);
+            }
+        }
+    }
+}
