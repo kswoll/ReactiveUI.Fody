7beb106d017e401448c784ff17e2e50983c2e437
Dominik Mydlil <dominik.mydlil@outlook.com>
2017-06-11 19:03:13 +0200

    Fix ObservableAsPropertyHelper of type long failing to compile (#48)
    The Ldc_I8 opcode (int64 value) requires an argument of type long,
    which was not being passed into the il.Emit function for
    ObservableAsPropertyHelpers of type long.

---
+++ ReactiveUI.Fody.Tests/Issues/Issue47Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI.Fody.Helpers;
+
+namespace ReactiveUI.Fody.Tests.Issues
+{
+	public class Issue47Tests
+	{
+		/// <summary>
+		/// The "test" here is simply for these to compile
+		/// Tests ObservableAsPropertyWeaver.EmitDefaultValue
+		/// </summary>
+		class TestModel : ReactiveObject
+		{
+			public extern int IntProperty { [ObservableAsProperty] get; }
+			public extern double DoubleProperty { [ObservableAsProperty] get; }
+			public extern float FloatProperty { [ObservableAsProperty] get; }
+			public extern long LongProperty { [ObservableAsProperty] get; }
+		}
+	}
+}
